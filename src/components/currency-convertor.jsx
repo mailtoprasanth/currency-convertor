fd4691fc34840b9b2d3dbe43d15f563c40cc7425
mailtoprasanth <166957588+mailtoprasanth@users.noreply.github.com>
2024-04-18 18:08:19 +0100

    Feature/auto build (#10)
    * test
    
    * test build
    
    * Feature/auto build (#9)
    
    * test build
    
    * test
    
    * ff
    
    * main yaml node version 20
    
    ---------
    
    Co-authored-by: prasanth <raj.prasanth1691@gmail.com>
    
    ---------
    
    Co-authored-by: prasanth <raj.prasanth1691@gmail.com>

--- src/components/currency-convertor.jsx
+++ src/components/currency-convertor.jsx
@@ -20,7 +20,9 @@ const CurrencyConvertor = () => {
   const [convertedAmount, setConvertedAmount] = useState(null);
   const [converting, setConverting] = useState(false);
 
-  const favHandler = (currency) => {};
+  const favHandler = (currency) => {
+    console.log(currency);
+  };
 
   const swapCurrencies = () => {
     setFromCurrency(toCurrency);
@@ -58,6 +60,10 @@ const CurrencyConvertor = () => {
       setConverting(false);
     }
   };
+  
+  useEffect(() => {
+    convert();
+  }, [toCurrency]);
 
   return (
     <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md">
